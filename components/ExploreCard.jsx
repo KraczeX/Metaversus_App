c21b90d46a00f51f4a9adf2f4ec55d1f17b5be8f
Krecik <bartoszkraczek2006@gmail.com>
2023-03-22 20:10:44 +0100

    update

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -20,12 +20,12 @@ const ExploreCard = ({id, imgUrl, title, index, active, handleClivk}) => (
       className='absolute w-full h-full object-cover rounded-[24px]'
     />
     {active !== id ? (
-      <h3 className='font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate[90deg] lg:origin[0,0]'>
+      <h3 className='font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin[0,0]'>
         {title}
       </h3>
     ) : (
-      <div>
-        <div className='absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]'>
+      <div className='absolute bottom-0 p-8'>
+        <div>
           <img 
           src='/headset.svg'
           alt="headset"
